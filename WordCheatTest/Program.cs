1d91eceb333d65f19d863fe36e4a39e8ff606863
bstanchev <bojidar.stanchev@siriushome.com>
2017-03-29 18:39:26 +0300

    initial work, test words and methods

---
+++ WordCheatTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCheatTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			char[,] array = new char[4, 4];
+			WordFinder finder = new WordFinder(array);
+			Console.ReadKey();
+		}
+
+		static void FillArrayWithRandom(ref char[,] array)
+		{
+			Random rand = new Random();
+			string abc = "abcdefghijklmnopqrstuvwxyz";
+
+			for(int i = 0; i < array.GetLength(0); i++)
+			{
+				for(int j = 0; j < array.GetLength(1); j++)
+				{
+					array[i, j] = abc[rand.Next(0, 25)];
+				}
+			}
+		}
+
+		static void PrintArray(char[,] array)
+		{
+			for(int i = 0; i < array.GetLength(0); i++)
+			{
+				for(int j = 0; j < array.GetLength(1); j++)
+				{
+					Console.Write(array[i, j] + " ");
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
---
+++ WordCheatTest/WordFinder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WordCheatTest
+{
+    class WordFinder
+    {
+		private char[,] matrix;
+		private string[] words;
+		private string[] dictionary;
+
+		public WordFinder(char[,] matrix)
+		{
+			this.matrix = matrix;
+			LoadDictionaryFromFile();	
+		}
+
+		private void LoadDictionaryFromFile()
+		{
+			dictionary = File.ReadAllLines("..\\..\\Dictionary.txt");
+		}
+
+		private bool IsWord(string str)
+		{
+			return dictionary.Contains(str);
+		}
+
+		private void SortWords()
+		{
+			words = words.OrderByDescending(word => word.Length).ToArray();
+		}
+
+		private void Search(string str = "")
+		{
+
+		}
+
+	}
+}
