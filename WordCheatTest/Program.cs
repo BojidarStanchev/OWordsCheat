2756773c2395ea62847184da567d30750ab45cf0
Bozhidar Stanchev <bojidarstanchev@gmail.com>
2017-04-02 17:08:14 +0300

    This seems to be working, needs optimization. I reduced finding time about 4 times by just limiting the word length to 9 characters, but it's still not enough, because a search for each starting point costs about 4 seconds and there are 16 starting points.

--- WordCheatTest/Program.cs
+++ WordCheatTest/Program.cs
@@ -10,7 +10,11 @@ namespace WordCheatTest
 	{
 		static void Main(string[] args)
 		{
-			char[,] array = new char[4, 4];
+			char[,] array = new char[4, 4] {	{'e', 'f', 'a', 'b' },
+												{'o', 'r', 'n', 'c' },
+												{'d', 'i', 'm', 'a' },
+												{'n', 'o', 'i', 't' }};
+
 			WordFinder finder = new WordFinder(array);
 			Console.ReadKey();
 		}
@@ -26,18 +30,6 @@ namespace WordCheatTest
 				{
 					array[i, j] = abc[rand.Next(0, 25)];
 				}
-			}
-		}
-
-		static void PrintArray(char[,] array)
-		{
-			for(int i = 0; i < array.GetLength(0); i++)
-			{
-				for(int j = 0; j < array.GetLength(1); j++)
-				{
-					Console.Write(array[i, j] + " ");
-				}
-				Console.WriteLine();
 			}
 		}
 	}
--- WordCheatTest/WordFinder.cs
+++ WordCheatTest/WordFinder.cs
@@ -7,21 +7,61 @@ using System.IO;
 
 namespace WordCheatTest
 {
-    class WordFinder
-    {
+		class WordFinder
+		{
 		private char[,] matrix;
-		private string[] words;
+		private bool[,] availabillityMap;
+
+		private List<string> words = new List<string>();
 		private string[] dictionary;
 
 		public WordFinder(char[,] matrix)
 		{
 			this.matrix = matrix;
-			LoadDictionaryFromFile();	
+			PrintMatrix(matrix);
+			LoadDictionaryFromFile();
+			InitializeAvailabillityMap();
+			Search();
+			SortWords();
+			PrintWords();
+		}
+
+		public void PrintMatrix(char[,] array)
+		{
+			for(int i = 0; i < array.GetLength(0); i++)
+			{
+				for(int j = 0; j < array.GetLength(1); j++)
+				{
+					Console.Write(array[i, j] + " ");
+				}
+				Console.WriteLine();
+			}
+		}
+
+		private void PrintWords()
+		{
+			for(int i = 0; i < words.Count; i++)
+			{
+				Console.WriteLine(words[i]);
+			}
 		}
 
 		private void LoadDictionaryFromFile()
 		{
 			dictionary = File.ReadAllLines("..\\..\\Dictionary.txt");
+		}
+
+		private void InitializeAvailabillityMap()
+		{
+			availabillityMap = new bool[matrix.GetLength(0), matrix.GetLength(1)];
+
+			for(int i = 0; i < matrix.GetLength(0); i++)
+			{
+				for (int j = 0; j < matrix.GetLength(1); j++)
+				{
+					availabillityMap[i, j] = true;
+				}
+			}
 		}
 
 		private bool IsWord(string str)
@@ -31,13 +71,49 @@ namespace WordCheatTest
 
 		private void SortWords()
 		{
-			words = words.OrderByDescending(word => word.Length).ToArray();
+			if(words.Count > 0)
+			{
+				words = words.OrderByDescending(word => word.Length).ToList();
+			}
 		}
-
-		private void Search(string str = "")
+		private void Search()
 		{
-
+			for(int i = 0; i < matrix.GetLength(0); i++)
+			{
+				for(int j = 0; j < matrix.GetLength(1); j++)
+				{
+					Search(i, j);
+				}
+			}
 		}
 
+		private void Search(int x, int y, string word = "")
+		{
+			if(word.Length > 9 || x < 0 || x > 3 || y < 0 || y > 3 || availabillityMap[x, y] == false)
+			{
+				return;
+			}
+
+			word += matrix[x, y];
+			availabillityMap[x, y] = false;
+
+			if(IsWord(word))
+			{
+				words.Add(word);
+			}
+
+			Search(x + 1, y, word);
+			Search(x - 1, y, word);
+			Search(x, y + 1, word);
+			Search(x, y - 1, word);
+			Search(x + 1, y + 1, word);
+			Search(x + 1, y - 1, word);
+			Search(x - 1, y + 1, word);
+			Search(x - 1, y - 1, word);
+
+			availabillityMap[x, y] = true;
+		
+			return;
+		}
 	}
 }
